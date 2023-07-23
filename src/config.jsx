78e4d5b64a518c3972563da9cdccf8b6c99e1585
Shubham Bishnoi <shubhambishnoi78@gmail.com>
2023-07-23 21:59:59 +0530

    typo: corrected twitter link

--- src/config.jsx
+++ src/config.jsx
@@ -1,6 +1,6 @@
 const linkedin = {icon:<i class="fa-brands fa-linkedin-in"></i>,link:"https://www.linkedin.com/in/bishnoishubham/"};
 const github = {icon:<i class="fa-brands fa-github"></i>,link:"https://github.com/shubhambishnoi"};
 const website = {icon: <i class="fa-solid fa-globe"></i>,link:"https://shubhambishnoi.netlify.app/"};
-const twitter = {icon:<i class="fa-brands fa-twitter"></i>,link:"https://twitter.com/shubham2220"};
+const twitter = {icon:<i class="fa-brands fa-twitter"></i>,link:"https://twitter.com/shubham_2220"};
 
 export {linkedin,github,website,twitter}
